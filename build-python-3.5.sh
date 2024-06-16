58c6d6be160cb11838a0916af2e06b90b7d00765
amidevous <amidevous@users.noreply.github.com>
2024-06-17 00:59:37 +0200

    Update build-python-3.5.sh

--- build-python-3.5.sh
+++ build-python-3.5.sh
@@ -6,20 +6,39 @@ TARGET=/usr/local/python3.5
 TARGET_ARCHIVE_DIR=/app/build
 TARGET_ARCHIVE_PATH=${TARGET_ARCHIVE_DIR}/python3.5-x86-64.tar.gz
 
-yum groupinstall -y "Development tools"
-
-yum install -y wget zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
+#actual RHEL 8 + (and fork)/Fedroa 21 +
+if test -f "/usr/bin/dnf"; then
+    sudo dnf groupinstall -y "Development tools"
+    sudo dnf install -y wget zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel libdb-devel libpcap-devel xz-devel expat-devel
+fi
+#obsolete CentOS 6/7
+if test -f "/usr/bin/yum"; then
+    sudo yum groupinstall -y "Development tools"
+    sudo yum install -y wget zlib-devel bzip2-devel openssl-devel ncurses-devel sqlite-devel readline-devel tk-devel gdbm-devel db4-devel libpcap-devel xz-devel expat-devel
+fi
+#OpenSuse
+if test -f "/usr/bin/zypper"; then
+    sudo zypper install --type pattern devel_basis -y
+    sudo zypper install -y wget zlib-devel bzip2 bzip3-devel libopenssl-devel ncurses-devel sqlite3-devel readline-devel tk-devel gdbm-devel libdb-4_8-devel libpcap-devel xz-devel libexpat-devel
+fi
+#Ubuntu/Debian
+if test -f "/usr/bin/apt-get"; then
+    sudo zypper install --type pattern devel_basis -y
+    sudo apt-get install -y wget zlib1g-dev bzip2 lbzip2 librust-bzip2-dev librust-bzip2-sys-dev libssl-dev ncurses-base ncurses-bin libncurses-dev sqlite3 libsqlite3-dev readline-common libreadline-dev tk-dev libgdbm-dev libdb-dev libpcap-dev xz-utils libexpat1-dev
+fi
 
 cd /tmp
 wget --no-check-certificate https://www.openssl.org/source/openssl-1.0.2n.tar.gz
 tar -xzf openssl-*.tar.gz
 cd openssl-*
-./config --prefix=/usr/local/openssl shared && make && make install
+./config --prefix=/usr/local/openssl shared
+make
+sudo make install
 
 mkdir -vp ${TARGET}
 
 cd /tmp
-wget --no-check-certificate https://www.python.org/ftp/python/3.5.5/Python-3.5.5.tgz
+wget --no-check-certificate https://www.python.org/ftp/python/3.5.10/Python-3.5.10.tgz
 tar -xzf Python-*.tgz
 cd Python-*
 ./configure --prefix=${TARGET} --with-thread --enable-unicode=ucs4 --enable-shared --enable-ipv6 --with-system-expat --with-system-ffi --with-signal-module
@@ -35,15 +54,16 @@ cp -av /usr/local/openssl/lib/engines .
 ln -vsf libcrypto.so.1.0.0 libcrypto.so.6
 ln -vsf libssl.so.1.0.0 libssl.so.6
 
-cp -av /lib64/libdb-4.7.so .
-ln -vsf libdb-4.7.so libdb.so
-cp -av /lib64/libreadline.so.6* .
-cp -av /lib64/libbz2.so* .
-cp -av /lib64/libcrypt-2.12.so /lib64/libcrypt.so.1 .
-cp -av /usr/lib64/libgdbm.so* .
-cp -av /usr/lib64/libsqlite3.so* .
-cp -av /lib64/libz.so* .
-cp -av /lib64/libncursesw.so* .
+
+find /usr -name 'libdb*.so' -exec cp -av {} . \;
+find /usr -name 'libreadline*.so*' -exec cp -av {} . \;
+find /usr -name 'libbz2*.so*' -exec cp -av {} . \;
+find /usr -name 'libcrypt*.so*' -exec cp -av {} . \;
+find /usr -name 'libgdbm*.so*' -exec cp -av {} . \;
+find /usr -name 'libsqlite*.so*' -exec cp -av {} . \;
+find /usr -name 'libz*.so*' -exec cp -av {} . \;
+find /usr -name 'libsqlite*.so*' -exec cp -av {} . \;
+find /usr -name 'libncursesw*.so*' -exec cp -av {} . \;
 
 find * -maxdepth 0 -name "*.so" -exec strip {} \;
 
